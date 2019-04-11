a1ff603ab1d9dd17c3b6b3d3aff23200f15c7d97
Pia Tello <PiaTello@MacBook-Pro.local>
2019-04-11 01:05:04 -0400

    changes

---
+++ public/js/members.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function (data) {
+    $(".member-name").text(data.email);
+
+  });
+
+  
+
+  });
+
+function onClick() {  
+  $("#add-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var newFlightSearch = {
+      originPlace: $("#originPlace").val().trim(),
+      destination: $("#destination").val().trim(),
+      outboundDate: $("#outboundDate").val().trim(),
+      returnDate: $("#returnDate").val().trim()
+    };
+
+    console.log(newFlightSearch);
+
+
+    $.ajax("/api/flights/" + newFlightSearch.originPlace + "/" + newFlightSearch.destination + "/" + newFlightSearch.outboundDate + "/" + newFlightSearch.returnDate, {
+      type: "GET",
+    }).then(function (userFlightInfo) {
+      console.log(userFlightInfo);
+
+      console.log(userFlightInfo.Places[0].CityName);
+       console.log(userFlightInfo.Carriers)
+   
+
+  
+       var fewQuotes = userFlightInfo.Quotes.slice(0, 6);
+      
+      for (let i = 0; i < fewQuotes.length; i++) {
+
+      var quoteDiv = $("<div class = 'col-xs-6'></div>");
+      var quote = fewQuotes[i]; 
+      var direct = quote.Direct;
+      var departureCity = userFlightInfo.Places[0].CityName;
+      var destinationCity = userFlightInfo.Places[1].CityName;
+      var outboundCarrierName = userFlightInfo.Carriers.find(c => c.CarrierId == quote.OutboundLeg.CarrierIds[0])
+      console.log('outboundCarrierName', outboundCarrierName);
+     var inboundCarrierName = userFlightInfo.Carriers.find(c => c.CarrierId == quote.InboundLeg.CarrierIds[0])
+      console.log('inboundCarrierName', inboundCarrierName);
+      var departureDate = quote.OutboundLeg.DepartureDate;
+      var returnDate= quote.InboundLeg.DepartureDate;
+      var minPrice = quote.MinPrice;
+
+
+      quoteDiv.append("Direct:" + " " + direct);
+      quoteDiv.append("Departure City:" + " " + departureCity);
+      quoteDiv.append("Destination City:" + " " + destinationCity);
+      quoteDiv.append("Departure Date:" + " " +departureDate);
+      quoteDiv.append("Return Date:" + ""+ returnDate);
+      quoteDiv.append("Direct:" + " " + direct);
+      quoteDiv.append("Outbound Carrier:" + " " + outboundCarrierName.Name);
+      quoteDiv.append("Inbound Carrier:" + " " + inboundCarrierName.Name);
+      quoteDiv.append("Price:" + " " + minPrice);
+      $("#quoteSection").append(quoteDiv);
+  
+      }
+              
+    }).catch(function (err) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  });
+
+};
+
+onClick ();
